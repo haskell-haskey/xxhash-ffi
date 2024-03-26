646bf2ddad55f93845fe5dcd088644fb598ba6ad
Bodigrim <andrew.lelechenko@gmail.com>
2024-03-26 21:22:29 +0000

    Benchmarks: make xxhash package (native Haskell implementation) an optional dependency

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Main where
 
@@ -6,7 +7,9 @@ import Prelude hiding (words)
 import qualified Data.ByteString as B
 import qualified Data.Digest.Adler32 as Adler32
 import qualified Data.Digest.CRC32 as CRC32
+#ifdef MIN_VERSION_xxhash
 import qualified Data.Digest.XXHash as XXHash
+#endif
 import qualified Data.Digest.XXHash.FFI as FFI
 import qualified Data.Hashable as Hashable
 
@@ -21,8 +24,10 @@ main = do
         [ bgroup "1MB"
             [ bench "Data.Digest.XXHash.FFI (32) (c)" $ nf (FFI.xxh32 med) 0
             , bench "Data.Digest.XXHash.FFI (64) (c)" $ nf (FFI.xxh64 med) 0
+#ifdef MIN_VERSION_xxhash
             , bench "xxhash (c)" $ nf XXHash.c_xxHash' med
             , bench "Data.Digest.XXHash (haskell)" $ nf XXHash.xxHash' med
+#endif
             , bench "Data.Digest.adler32 (c, zlib)" $ nf Adler32.adler32 med
             , bench "Data.Digest.crc32 (c, zlib)" $ nf CRC32.crc32 med
             , bench "Data.Hashable (c, FNV?)" $ nf Hashable.hash med
@@ -30,8 +35,10 @@ main = do
         , bgroup "1kB"
             [ bench "Data.Digest.XXHash.FFI (32) (c)" $ nf (FFI.xxh32 bs1k) 0
             , bench "Data.Digest.XXHash.FFI (64) (c)" $ nf (FFI.xxh64 bs1k) 0
+#ifdef MIN_VERSION_xxhash
             , bench "xxhash (c)" $ nf XXHash.c_xxHash' bs1k
             , bench "Data.Digest.XXHash (haskell)" $ nf XXHash.xxHash' bs1k
+#endif
             , bench "Data.Digest.adler32 (c, zlib)" $ nf Adler32.adler32 bs1k
             , bench "Data.Digest.crc32 (c, zlib)" $ nf CRC32.crc32 bs1k
             , bench "Data.Hashable (c, FNV?)" $ nf Hashable.hash bs1k
@@ -39,8 +46,10 @@ main = do
         , bgroup "4kB"
             [ bench "Data.Digest.XXHash.FFI (32) (c)" $ nf (FFI.xxh32 bs4k) 0
             , bench "Data.Digest.XXHash.FFI (64) (c)" $ nf (FFI.xxh64 bs4k) 0
+#ifdef MIN_VERSION_xxhash
             , bench "xxhash (c)" $ nf XXHash.c_xxHash' bs4k
             , bench "Data.Digest.XXHash (haskell)" $ nf XXHash.xxHash' bs4k
+#endif
             , bench "Data.Digest.adler32 (c, zlib)" $ nf Adler32.adler32 bs4k
             , bench "Data.Digest.crc32 (c, zlib)" $ nf CRC32.crc32 bs4k
             , bench "Data.Hashable (c, FNV?)" $ nf Hashable.hash bs4k
