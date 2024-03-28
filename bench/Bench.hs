6622d470f4600ca1aa706d33ff88d1b3375ff49e
Bodigrim <andrew.lelechenko@gmail.com>
2024-03-28 00:45:23 +0000

    Extend benchmarks

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -24,17 +24,18 @@ main = do
     med <- B.readFile "/usr/share/dict/words"
 
     defaultMain
-        [ bgroup "1MB"
-            [ bench "Data.Digest.XXHash.FFI (32) (c)" $ nf (FFI.xxh32 med) 0
-            , bench "Data.Digest.XXHash.FFI (64) (c)" $ nf (FFI.xxh64 med) 0
+        [ bgroup "10B"
+            [ bench "Data.Digest.XXHash.FFI (32) (c)" $ nf (FFI.xxh32 bs10b) 0
+            , bench "Data.Digest.XXHash.FFI (64) (c)" $ nf (FFI.xxh64 bs10b) 0
 #ifdef MIN_VERSION_xxhash
-            , bench "xxhash (c)" $ nf XXHash.c_xxHash' med
-            , bench "Data.Digest.XXHash (haskell)" $ nf XXHash.xxHash' med
+            , bench "xxhash (c)" $ nf XXHash.c_xxHash' bs10b
+            , bench "Data.Digest.XXHash (haskell)" $ nf XXHash.xxHash' bs10b
 #endif
-            , bench "Data.Digest.adler32 (c, zlib)" $ nf Adler32.adler32 med
-            , bench "Data.Digest.crc32 (c, zlib)" $ nf CRC32.crc32 med
+            , bench "Data.Digest.adler32 (c, zlib)" $ nf Adler32.adler32 bs10b
+            , bench "Data.Digest.crc32 (c, zlib)" $ nf CRC32.crc32 bs10b
 #ifdef MIN_VERSION_hashable
-            , bench "Data.Hashable (c, FNV?)" $ nf Hashable.hash med
+            , bench "Data.Digest.XXHash (c, XXH3)" $ nf Hashable.hash (FFI.XXH3 bs10b)
+            , bench "Data.Hashable (c, FNV?)" $ nf Hashable.hash bs10b
 #endif
             ]
         , bgroup "1kB"
@@ -47,6 +48,7 @@ main = do
             , bench "Data.Digest.adler32 (c, zlib)" $ nf Adler32.adler32 bs1k
             , bench "Data.Digest.crc32 (c, zlib)" $ nf CRC32.crc32 bs1k
 #ifdef MIN_VERSION_hashable
+            , bench "Data.Digest.XXHash (c, XXH3)" $ nf Hashable.hash (FFI.XXH3 bs1k)
             , bench "Data.Hashable (c, FNV?)" $ nf Hashable.hash bs1k
 #endif
             ]
@@ -60,10 +62,28 @@ main = do
             , bench "Data.Digest.adler32 (c, zlib)" $ nf Adler32.adler32 bs4k
             , bench "Data.Digest.crc32 (c, zlib)" $ nf CRC32.crc32 bs4k
 #ifdef MIN_VERSION_hashable
+            , bench "Data.Digest.XXHash (c, XXH3)" $ nf Hashable.hash (FFI.XXH3 bs4k)
             , bench "Data.Hashable (c, FNV?)" $ nf Hashable.hash bs4k
 #endif
             ]
+        , bgroup "1MB"
+            [ bench "Data.Digest.XXHash.FFI (32) (c)" $ nf (FFI.xxh32 med) 0
+            , bench "Data.Digest.XXHash.FFI (64) (c)" $ nf (FFI.xxh64 med) 0
+#ifdef MIN_VERSION_xxhash
+            , bench "xxhash (c)" $ nf XXHash.c_xxHash' med
+            , bench "Data.Digest.XXHash (haskell)" $ nf XXHash.xxHash' med
+#endif
+            , bench "Data.Digest.adler32 (c, zlib)" $ nf Adler32.adler32 med
+            , bench "Data.Digest.crc32 (c, zlib)" $ nf CRC32.crc32 med
+#ifdef MIN_VERSION_hashable
+            , bench "Data.Digest.XXHash (c, XXH3)" $ nf Hashable.hash (FFI.XXH3 med)
+            , bench "Data.Hashable (c, FNV?)" $ nf Hashable.hash med
+#endif
+            ]
         ]
+
+bs10b :: B.ByteString
+bs10b = fst $ B.unfoldrN 10 (\x -> Just (x, x)) 0
 
 bs1k :: B.ByteString
 bs1k = fst $ B.unfoldrN 1024 (\x -> Just (x, x)) 0
